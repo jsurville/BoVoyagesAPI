7aa9fd602d1d7aa4b80c0deae761b2a49f5df4c8
toto <titi>
2018-09-20 15:48:49 +0200

    Fonctinalite annuler dossier reservationOk

---
+++ BoVoyagesAPI/Metier/CarteBancaireService.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoVoyagesAPI.Metier
+{
+    class CarteBancaireService
+    {
+        public bool ValiderSolvabilite(string numeroCarteBancaire, decimal prixTotal)
+        {
+            try
+            {
+                return ((int.Parse(numeroCarteBancaire) -(int) prixTotal) % 2) == 0;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        public bool Rembourser(string numeroCarteBancaire, decimal prixTotal)
+        {
+            return true ;
+        }
+
+        public bool PayerAgence(object p)
+        {
+            return true ;
+        }
+    }
+}
